c8b5e96fe298234ef22998ccb864169b8b65a2ab
Alptuğ <alptugidin@gmail.com>
2022-11-03 03:33:09 +0300

    convert svgs to react component

---
+++ src/lib/components/Svg/BackSVG.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const BackSVG = (): JSX.Element => {
+  return (
+    <svg
+      width='24'
+      height='24'
+      fill='#1a73e8'
+      focusable="false"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24">
+      <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path>
+    </svg>
+  );
+};
+
+export default BackSVG;
