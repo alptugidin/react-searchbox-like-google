8662ab4b74c3a97282ba26d8d4812439a4d8c885
Alptuğ <alptugidin@gmail.com>
2022-11-04 05:12:02 +0300

    add new properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, useState } from 'react';
+import React, { CSSProperties, useEffect, useState } from 'react';
 // import { SearchBox } from './lib';
 import { useFetch } from './hooks/useFetchMovies';
 import { IOnClickData } from './lib/components/SearchBox/types';
@@ -26,37 +26,32 @@ const App = (): JSX.Element => {
 
   return (
     <div className={`flex justify-center h-screen relative ${!darkMode ? 'bg-white' : 'bg-[#202124]'}`}>
-      <div className='absolute right-5 top-5'>
+      <div className='absolute right-5 bottom-5 rounded-full drop-shadow-md'>
         <button
+          id='nightMode'
           type='button'
           onClick={() => setDarkMode(!darkMode)}
-          className='text-white bg-red-600 hover:bg-red-500 transition-all w-44 px-4 py-2 rounded-full font-bold hidden'
+          className='text-white bg-red-600 hover:bg-red-500 transition-all w-44 px-4 py-2 rounded-full font-bold'
         >
           Dark Mode {darkMode ? 'On' : ' Off'}
         </button>
       </div>
       <div className='w-[500px] mt-20'>
-        {/* <SearchBox
+        <SearchBox
           onChange={handleOnChange}
           onClick={handleOnclick}
           results={results}
+          // showImage
+          // showDetail
           darkMode={darkMode}
           buttons={[
             { label: 'Search', handler: btn1handler },
             { label: 'Do something', handler: btn2handler }
           ]}
-        />
-      </div> */}
-        <SearchBox
-          onChange={handleOnChange}
-          onClick={handleOnclick}
-          results={results}
-          showDetail
-          showImage
-          buttons={[
-            { label: 'Search', handler: btn1handler },
-            { label: 'Do something', handler: btn2handler }
-          ]}
+          // colors={{
+          //   darkPrimary: '#202124', // default #202124
+          //   darkSecondary: '#303134' // default #303134
+          // }}
         />
       </div>
     </div>
