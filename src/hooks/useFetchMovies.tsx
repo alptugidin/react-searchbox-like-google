b925fe6afdf97bb42181e0478d3af6529539f3d2
Alptuğ <alptugidin@gmail.com>
2022-11-06 05:12:41 +0300

    fix

--- src/hooks/useFetchMovies.tsx
+++ src/hooks/useFetchMovies.tsx
@@ -14,11 +14,12 @@ export const useFetch = (param: string): IFetchApiResponse => {
   const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    const controller = new AbortController();
+    // const controller = new AbortController();
     if (param !== undefined && param.length > 0) {
       const arr: ISearchResults[] = [];
       axios.get(`https://api.themoviedb.org/3/search/movie?api_key=204f2cebe811d76c47a873f7233cf17a&language=en-US&query=${param}&page=1&include_adult=false`
-        , { signal: controller.signal }).then(response => {
+        // , { signal: controller.signal }).then(response => {
+      ).then(response => {
         setLoading(true);
         response.data.results.forEach((data: any) => {
           const el: ISearchResults = {
@@ -39,7 +40,7 @@ export const useFetch = (param: string): IFetchApiResponse => {
       });
     }
 
-    return () => controller.abort();
+    // return () => controller.abort();
   }, [param]);
   return { asyncResults, error, loading };
 };
