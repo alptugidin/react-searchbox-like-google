b276d7266a4c74d4c05aa7625ae35030a2a318d2
Alptuğ <alptugidin@gmail.com>
2022-11-07 05:05:43 +0300

    fix

--- src/lib/utils/filterCondition.ts
+++ src/lib/utils/filterCondition.ts
@@ -1,7 +1,9 @@
 import { ISearchResults } from '../components/SearchBox/types';
 
-export const filterCondition = (param: ISearchResults, value: string): any => {
+export const filterCondition = (param: ISearchResults, value: string): ISearchResults => {
+  let output;
   if (param.title.toLowerCase().includes(value.toLocaleLowerCase())) {
-    return param;
+    output = param;
   }
+  return output as ISearchResults;
 };
