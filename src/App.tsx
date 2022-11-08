59fedfc25902df9446cae3e0eec42f85c4498314
Alptuğ <alptugidin@gmail.com>
2022-11-08 05:21:48 +0300

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,58 @@
-import React, { CSSProperties, useEffect, useState } from 'react';
+import React, { useState, memo, CSSProperties } from 'react';
 import { useFetch } from './hooks/useFetchMovies';
 import { IOnClickData, ISearchResults } from './lib/components/SearchBox/types';
 import { SearchBox } from './lib';
+
 const App = (): JSX.Element => {
   const [query, setQuery] = useState<string>();
   const [darkMode, setDarkMode] = useState(false);
+
   const { asyncResults, error, loading } = useFetch(query as string);
-  const results: ISearchResults[] = [
-    { id: 1, title: 'Javascript language' },
-    { id: 2, title: 'Java language' },
-    { id: 3, title: 'Kotlin language' },
-    { id: 4, title: 'Swift language' },
-    { id: 5, title: 'Dart language' },
-    { id: 6, title: 'Pythın language' },
-    { id: 7, title: 'Rust Language' }
-  ];
+  const [results, setResults] = useState<ISearchResults[] >(
+    [
+      {
+        id: 1,
+        title: 'Javascript language',
+        detail: 'Lorem ipsum dolor sit amet.',
+        image: 'https://www.sistemkod.com/images/blog/javascriptlogo.png'
+      },
+      { id: 2, title: 'Java language' },
+      {
+        id: 3,
+        title: 'Kotlin language',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/7/74/Kotlin_Icon.png'
+      },
+      { id: 4, title: 'Swift language' },
+      { id: 5, title: 'Dart language' },
+      { id: 6, title: 'Pythın language' },
+      {
+        id: 7,
+        title: 'Rust Language',
+        detail: 'Lorem ipsum dolor sit amet.'
+      }
+    ]
+  );
+
   const handleOnclick = (onClickData: IOnClickData): void => {
-    // window.open(onClickData.href, '_blank');
+    // you can use react router in here
   };
 
   const handleOnChange = (onChangeData: string): void => {
     setQuery(onChangeData);
   };
 
+  const handleOnclick2 = (): void => {
+    // do something
+  };
+
+  const handleOnChange2 = (onChangeData: string): void => {
+    // you can also use with onChangeData parameter. it is presents click data.
+  };
+
   const btn1handler = (onClickData: IOnClickData): void => {
     console.log('Button 1 clicked!');
     console.log(onClickData);
+    //
   };
 
   const btn2handler = (): void => {
@@ -35,30 +62,54 @@ const App = (): JSX.Element => {
   return (
     <div
       className={`flex justify-center h-screen relative transition-all ${!darkMode ? 'bg-white' : 'bg-[#202124]'}`}>
-      <div className='absolute right-5 bottom-5 rounded-full drop-shadow-md flex gap-10'>
-        <button
-          id='nightMode'
-          type='button'
-          onClick={() => setDarkMode(!darkMode)}
-          className='text-white bg-red-600 hover:bg-red-500 transition-all w-44 px-4 py-2 rounded-full font-bold'
-        >
-          Dark Mode {darkMode ? 'On' : ' Off'}
-        </button>
-      </div>
-      <div className='w-[500px] mt-56'>
-        <SearchBox
-          onChange={handleOnChange}
-          onClick={handleOnclick}
-          results={asyncResults}
-          darkMode={darkMode}
-          buttons={[
-            { label: 'Search', handler: btn1handler },
-            { label: 'Do something', handler: btn2handler }
-          ]}
+
+      <button
+        type='button'
+        onClick={() => setDarkMode(!darkMode)}
+        className='absolute right-5 top-5 w-14 h-14 rounded-full bg-red-600 overflow-hidden'>
+        <img
+          src="/sun.svg"
+          alt="sun"
+          className={`absolute right-0 left-0 ml-auto mr-auto transition-all ${darkMode ? '-bottom-[38px]' : 'bottom-[16px]'}`}
         />
+        <img
+          src="/moon.svg"
+          alt="moon"
+          className={`absolute right-0 left-0 ml-auto mr-auto transition-all ${darkMode ? 'bottom-[16px]' : '-bottom-[38px]'}`}
+        />
+      </button>
+      <div className='mt-56 flex gap-20'>
+        <div className='w-[400px]'>
+          <SearchBox
+            onChange={handleOnChange}
+            onClick={handleOnclick}
+            results={asyncResults}
+            darkMode={darkMode}
+            showImage
+            showDetail
+            buttons={[
+              { label: 'Search', handler: btn1handler },
+              { label: 'Do something', handler: btn2handler }
+            ]}
+          />
+        </div>
+        <div className='w-[400px]'>
+          <SearchBox
+            onChange={handleOnChange2}
+            onClick={handleOnclick2}
+            results={results}
+            darkMode={darkMode}
+            showDetail
+            showImage
+            buttons={[
+              { label: 'Search', handler: btn1handler },
+              { label: 'Do something', handler: btn2handler }
+            ]}
+          />
+        </div>
       </div>
     </div>
   );
 };
 
-export default App;
+export default memo(App);
