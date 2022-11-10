8b5ba7523af35b7f374d33f7eaffa338f1a53621
Alptuğ <alptugidin@gmail.com>
2022-11-10 04:50:12 +0300

    improve error handling and add skeleton for img

--- src/lib/components/SearchBox/ImageLoader/imageLoader.tsx
+++ src/lib/components/SearchBox/ImageLoader/imageLoader.tsx
@@ -1,35 +1,36 @@
-import { SearchSVG } from 'lib/components/Svg';
-import React, { Fragment, FunctionComponent, useState } from 'react';
 import style from '../SearchBox.module.scss';
+import React, { Fragment, FunctionComponent, useState } from 'react';
+import { SearchSVG } from 'lib/components/Svg';
 interface IImageLoader {
     showImage: boolean | undefined
     data: any
 }
 const ImageLoader: FunctionComponent<IImageLoader> = (props): JSX.Element => {
   const [imgError, setImgError] = useState(false);
+  const [isLoad, setIsLoad] = useState(false);
   const handleError = (currentTarget: HTMLImageElement): void => {
     setImgError(true);
     currentTarget.style.display = 'none';
   };
+
+  const handleLoad = (): void => {
+    setIsLoad(true);
+  };
+
   return (
     (props.showImage && (props.data.image !== '' && props.data.image !== undefined))
       ? (
         <Fragment>
-          {!imgError
-            ? (
-              <div className={style.sb_result_image}>
-                <img
-                  onError={({ currentTarget }) => handleError(currentTarget)}
-                  src={props.data.image}
-                  alt="img" />
-              </div>
-            )
-            : (
-
-              <div className={style.sb_result_svg}>
-                <SearchSVG/>
-              </div>
-            )}
+          <div className={imgError ? style.sb_result_svg : style.sb_result_image}>
+            <img
+              onError={({ currentTarget }) => handleError(currentTarget)}
+              onLoad={handleLoad}
+              style={{ display: isLoad ? 'block' : 'none' }}
+              src={props.data.image}
+              alt="poster" />
+          </div>
+          {imgError && <SearchSVG/>}
+          {!isLoad && !imgError && <div className={style.img_skeleton}></div>}
         </Fragment>
       )
       : (
@@ -37,6 +38,7 @@ const ImageLoader: FunctionComponent<IImageLoader> = (props): JSX.Element => {
           <SearchSVG/>
         </div>
       )
+
   );
 };
 
