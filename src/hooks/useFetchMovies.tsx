4c9320c53591f5d308a65df9440a55c2c3bef2cb
Alptuğ <alptugidin@gmail.com>
2022-11-11 05:10:21 +0300

    fix

--- src/hooks/useFetchMovies.tsx
+++ src/hooks/useFetchMovies.tsx
@@ -18,8 +18,8 @@ export const useFetch = (param: string): IFetchApiResponse => {
     if (param !== undefined && param.length > 0) {
       const arr: ISearchResults[] = [];
       axios.get(`https://api.themoviedb.org/3/search/movie?api_key=204f2cebe811d76c47a873f7233cf17a&language=en-US&query=${param}&page=1&include_adult=false`
-        // , { signal: controller.signal }).then(response => {
-      ).then(response => {
+        , { signal: controller.signal }).then(response => {
+      // ).then(response => {
         setLoading(true);
         response.data.results.forEach((data: any) => {
           const el: ISearchResults = {
@@ -40,7 +40,7 @@ export const useFetch = (param: string): IFetchApiResponse => {
       });
     }
 
-    // return () => controller.abort();
+    return () => controller.abort();
   }, [param]);
   return { asyncResults, error, loading };
 };
