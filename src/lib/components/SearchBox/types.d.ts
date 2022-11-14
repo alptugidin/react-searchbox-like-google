cb2b5e21af9336e8722786404293d32e2625eba2
Alptuğ <alptugidin@gmail.com>
2022-11-14 05:02:30 +0300

    fix typos

--- src/lib/components/SearchBox/types.d.ts
+++ src/lib/components/SearchBox/types.d.ts
@@ -1,7 +1,7 @@
 export interface ISearchBoxProps {
   onChange: (onChangeData: string) => void
-  onClick: (onClickData: IOnclickData) => void
-  results: ISearchResult[] | undefined
+  onClick: (onClickData: IOnClickData) => void
+  results: ISearchResults[] | undefined
   limit?: number
   thresHold?: number
   placeHolder?: string
@@ -21,39 +21,6 @@ export interface ISearchBoxProps {
   ]
 }
 
-export interface IProviderValues extends ISearchBoxProps {
-  arr: ISearchResults[] | undefined
-  setArr: React.Dispatch<React.SetStateAction<ISearchResults[] | undefined>>
-  value: string
-  setValue: React.Dispatch<React.SetStateAction<string>>
-  tempVal: string
-  setTempVal: React.Dispatch<React.SetStateAction<string>>
-  active: number
-  setActive: React.Dispatch<React.SetStateAction<number>>
-  mainRef: React.RefObject<HTMLDivElement>
-  inputRef: React.RefObject<HTMLInputElement>
-  topRef: React.RefObject<HTMLDivElement>
-  dropdownRef: React.RefObject<HTMLDivElement>
-
-}
-
-export interface ISearchBoxColors {
-  text?: string
-  highlightText?: string
-  darkTheme?: string
-}
-
-export interface ISearchBoxButtons {
-  [
-    btn1?: { label: string, handler: (onChangeData: IOnClickData) => void },
-    btn2?: { label: string, handler: (onChangeData: IOnClickData) => void },
-  ]
-}
-
-export interface IButtonHandlers {
-  label: string
-  handler: (onClickData: IOnclickData) => void
-}
 export interface ISearchResults {
   id: number
   title: string
@@ -61,7 +28,7 @@ export interface ISearchResults {
   href?: string
   detail?: string
 }
-export interface IOnClickData extends ISearchResult { }
+export interface IOnClickData extends ISearchResults { }
 
 export interface ISearchResultsProps extends Pick<ISearchBoxProps,
  'darkMode' | 'showImage' | 'showDetail' | 'buttons' | 'sx'
