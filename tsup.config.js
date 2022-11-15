358db597a6689a55cdd89ecd5b4c18012dd95316
Alptuğ <alptugidin@gmail.com>
2022-11-15 05:25:41 +0300

    add new rules

--- tsup.config.js
+++ tsup.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'tsup';
 import { sassPlugin } from 'esbuild-sass-plugin'
 export default defineConfig({
-    // entry: ['./src/lib/index.ts'],
     entry: ['./src/lib/index.ts'],
     format: ['cjs', 'esm'],
-    dts: true,
+    // dts: { resolve: "./src/lib/components/SearchBox/types.d.ts" },
+    // dts: true,
+    clean: true,
     legacyOutput: true,
     loader: { '.scss': 'css' },
     esbuildPlugins: [sassPlugin()]
