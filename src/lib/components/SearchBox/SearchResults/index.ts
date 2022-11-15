6a2e92f7b285c164a5c9acc8d7f63b1196a63e17
Alptuğ <alptugidin@gmail.com>
2022-11-15 05:27:42 +0300

    fix export

--- src/lib/components/SearchBox/SearchResults/index.ts
+++ src/lib/components/SearchBox/SearchResults/index.ts
@@ -1,2 +1,4 @@
-import SearchResults from './SearchResults';
-export default SearchResults;
+// import SearchResults from './SearchResults';
+// export default SearchResults;
+
+export { default } from './SearchResults';
