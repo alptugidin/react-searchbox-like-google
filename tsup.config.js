cd905f0f8d99b90f0a003140ad48fda5d5a34664
Alptuğ <alptugidin@gmail.com>
2022-11-16 21:27:19 +0300

    add new options

--- tsup.config.js
+++ tsup.config.js
@@ -3,10 +3,12 @@ import { sassPlugin } from 'esbuild-sass-plugin'
 export default defineConfig({
     entry: ['./src/lib/index.ts'],
     format: ['cjs', 'esm'],
-    // dts: { resolve: "./src/lib/components/SearchBox/types.d.ts" },
-    // dts: true,
+    dts: { entry: { "index": "./src/lib/components/SearchBox/types.d.ts" } },
+    minify: true,
     clean: true,
-    legacyOutput: true,
     loader: { '.scss': 'css' },
-    esbuildPlugins: [sassPlugin()]
+    esbuildPlugins: [sassPlugin()],
+    outExtension({ format }) {
+        return format === 'esm' ? { js: `.${format}.js` } : { js: `.js` }
+    }
 });
